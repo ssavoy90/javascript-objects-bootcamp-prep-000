512b918fc73c435e67265406f7ef5f0b5f1a6c88
Shanelle Savoy <mznellienellz@gmail.com>
2018-05-31 17:27:17 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,8 @@ function updatePlaylist(playlist, artist, song) {
   playlist[artist] = song;
   return playlist
 }
+
+function removeFromPlaylist(playlist, artist) {
+  delete artist.song
+  return playlist
+}
